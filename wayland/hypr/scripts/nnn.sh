f70fd390d59914a25e448b1c5aed953aefdb9334
Raxen001 <raxen001@tutamail.com>
2023-05-21 01:38:00 +0530

    fixed nnn.sh

--- wayland/hypr/scripts/nnn.sh
+++ wayland/hypr/scripts/nnn.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 # author: RITHESH S <raxen001@tutamail.com>
-# TODO: make nnn open on a special workspace and move the nnn to current actual workspace
-# press again to put it in special workspace and hide it
-TOGGLE="/tmp/nnnterm"
+TOGGLE="/tmp/nnnterm.lock"
 if [[  $(hyprctl clients -j | jq  "[.[]? | .class | contains(\"nnnterm\")] | any") == "false" ]];then
     hyprctl --batch "dispatchers exec [float] alacritty --class nnnterm -e nnn;dispatchers focuswindow nnnterm"
     touch $TOGGLE
 elif [ -f "$TOGGLE" ];then
-    hyprctl dispatchers "movetoworkspacesilent special:nnn,nnnterm" 
+    hyprctl --batch "dispatchers togglefloating ^(nnnterm)\$;dispatchers movetoworkspace special,^(nnnterm)\$" 
     rm "$TOGGLE"
 else
-    hyprctl --batch "dispatchers movetoworkspacesilent e+0,nnnterm;dispatchers focuswindow nnnterm"
+    hyprctl --batch "dispatchers movetoworkspace +0,^(nnnterm)\$;dispatchers togglefloating ^(nnnterm)\$; dispatchers focuswindow ^(nnnterm)\$; dispatchers centerwindow"
     touch $TOGGLE
 fi
 
