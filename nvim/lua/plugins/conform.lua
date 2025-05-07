8201a0ce69d0504f5b229b204f82c39abbafd2e5
raxen001 <raxen001@tutamail.com>
2025-05-07 13:03:25 +0530

    added conform nvim

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,30 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		-- setup conform for the following languages.
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "black" },
+				rust = { "rustfmt", lsp_format = "fallback" },
+				javascript = { "prettier", "prettierd", stop_after_first = true },
+                javascriptreact = { "prettier" },
+				php = { "phpcbf" },
+			},
+		})
+
+		-- setup :Format command for conform
+		vim.api.nvim_create_user_command("Format", function(args)
+			local range = nil
+			if args.count ~= -1 then
+				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+				range = {
+					start = { args.line1, 0 },
+					["end"] = { args.line2, end_line:len() },
+				}
+			end
+			require("conform").format({ async = true, lsp_format = "fallback", range = range })
+		end, { range = true })
+	end,
+}
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,13 +1,19 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  -- opts = {
-  --   -- your configuration comes here
-  --   -- or leave it empty to use the default settings
-  --   -- refer to the configuration section below
-  -- },
-  -- keys = {
-  --   {
-  --   },
-  -- },
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    config = function()
+        local wk = require("which-key")
+        wk.add({
+            { "<leader>lf", "<cmd>Format<cr>", desc = "Format file", mode = "n" },
+        })
+    end,
+    -- opts = {
+    --   -- your configuration comes here
+    --   -- or leave it empty to use the default settings
+    --   -- refer to the configuration section below
+    -- },
+    -- keys = {
+    --   {
+    --   },
+    -- },
 }
