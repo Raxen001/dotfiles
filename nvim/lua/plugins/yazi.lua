e797f104aa5246b9f709f7190f46e47c1c271a74
raxen001 <raxen001@tutamail.com>
2025-06-05 13:25:44 +0530

    feat: yazi open on directory

--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -19,7 +19,7 @@ return {
 	---@type YaziConfig | {}
 	opts = {
 		-- if you want to open yazi instead of netrw, see below for more info
-		open_for_directories = false,
+		open_for_directories = true,
 		keymaps = {
 			show_help = "<f1>",
 		},
@@ -27,7 +27,7 @@ return {
 	-- 👇 if you use `open_for_directories=true`, this is recommended
 	init = function()
 		-- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
-		-- vim.g.loaded_netrw = 1
+		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
 	end,
 }
