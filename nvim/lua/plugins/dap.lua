e57306e0638b01a56a86087eefa5d55d5c3508e9
raxen001 <raxen001@tutamail.com>
2025-07-06 00:45:27 +0530

    feat: added dap in nvim; added zen about:configs in readme.md

---
+++ nvim/lua/plugins/dap.lua
@@ -0,0 +1,86 @@
+return {
+	"mfussenegger/nvim-dap",
+	lazy = true,
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"jay-babu/mason-nvim-dap.nvim",
+		"nvim-neotest/nvim-nio",
+		"theHamsta/nvim-dap-virtual-text",
+	},
+	keys = {
+		{
+			"<F3>",
+			function()
+				require("dap").toggle_breakpoint()
+			end,
+			desc = "Debugger Breakpoint",
+		},
+		{
+			"<Up>",
+			function()
+				require("dap").continue()
+			end,
+			desc = "Debugger Continue",
+		},
+		{
+			"<Left>",
+			function()
+				require("dap").step_out()
+			end,
+			desc = "Debugger Step Out",
+		},
+		{
+			"<Right>",
+			function()
+				require("dap").step_into()
+			end,
+			desc = "Debugger Step Into",
+		},
+		{
+			"<Down>",
+			function()
+				require("dap").step_over()
+			end,
+			desc = "Debugger Step Over",
+		},
+		{
+			"<leader>dl",
+			function()
+				require("dap").list_breakpoints()
+				vim.cmd("copen")
+			end,
+			desc = "Debugger List Breakpoints",
+		},
+		{
+			"<leader>du",
+			function()
+				require("dapui").toggle()
+			end,
+			desc = "Debugger UI",
+		},
+	},
+	config = function()
+		require("dapui").setup()
+		require("mason-nvim-dap").setup()
+		require("nvim-dap-virtual-text").setup()
+		require("mason-nvim-dap").setup({
+			ensure_installed = { "python", "php" },
+			handlers = {},
+		})
+
+		local dap, dapui = require("dap"), require("dapui")
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+	end,
+}
