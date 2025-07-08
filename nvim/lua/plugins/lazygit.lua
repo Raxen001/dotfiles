001276403776c5005578b91bc73bf00288030ec3
RITHESH S <rithesh.satheesh@rtcamp.com>
2025-07-08 11:57:59 +0530

    feeat: added telescope lazygit

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -9,9 +9,13 @@ return {
         "LazyGitFilterCurrentFile",
     },
     dependencies = {
+        "nvim-telescope/telescope.nvim",
         "nvim-lua/plenary.nvim",
     },
     keys = {
         { "<leader>g", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-    }
+    },
+    config = function()
+        require("telescope").load_extension("lazygit")
+    end,
 }
