28b4f1156f5e01f9fda45e449cc00296d80a9f34
raxen001 <raxen001@tutamail.com>
2025-07-29 12:57:53 +0530

    feat: signcolumn

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -16,6 +16,7 @@ vim.opt.relativenumber = true       -- add numbers to each line on the left side
 vim.opt.cursorline = true           -- highlight cursor line underneath the cursor horizontally
 vim.opt.textwidth = 80
 vim.opt.laststatus = 3              -- global status line instead of individual windows
+vim.opt.signcolumn = "yes"           -- sign column for lsp warning
 -- vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg=0, bg=LightGrey })
 -- split
 vim.opt.splitbelow = true           -- open new vertical split bottom
